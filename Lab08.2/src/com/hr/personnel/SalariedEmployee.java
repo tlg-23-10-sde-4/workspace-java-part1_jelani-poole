fcb812f88167fac774fe7a290a4472df15fb9c5d
Jelani Poole <jelani.poole@tlgcohort.com>
2023-10-25 09:37:25 -0700

    Executive, takeVacation

--- Lab08.2/src/com/hr/personnel/SalariedEmployee.java
+++ Lab08.2/src/com/hr/personnel/SalariedEmployee.java
@@ -28,6 +28,10 @@ public class SalariedEmployee extends Employee {
         System.out.println(getName() + " is paid salary " + payment);
     }
 
+    public void takeVacation() {
+        System.out.println(getName() + " is on vacation");
+    }
+
     //accessor methods
     public double getSalary() {
         return salary;
--- Lab08.2/src/com/hr/personnel/client/HRClient.java
+++ Lab08.2/src/com/hr/personnel/client/HRClient.java
@@ -8,10 +8,7 @@
 
 package com.hr.personnel.client;
 
-import com.hr.personnel.Department;
-import com.hr.personnel.Employee;
-import com.hr.personnel.HourlyEmployee;
-import com.hr.personnel.SalariedEmployee;
+import com.hr.personnel.*;
 
 import java.time.LocalDate;
 
@@ -21,6 +18,7 @@ import java.time.LocalDate;
 class HRClient {
 
     public static void main(String[] args) {
+
         // create Department object
         Department dept = new Department("Sales", "Seattle");
         System.out.println(dept); // toString auto called
@@ -28,9 +26,9 @@ class HRClient {
         // add Employees to it
         dept.addEmployee(new SalariedEmployee("Jason", LocalDate.of(1990, 8, 24), 1175.0));
         dept.addEmployee(new SalariedEmployee("Julie", LocalDate.of(2000, 2, 2), 1275.0));
-        dept.addEmployee(new SalariedEmployee("Fred",  LocalDate.of(2015, 10, 19), 1500.0));
+        dept.addEmployee(new HourlyEmployee("Fred",  LocalDate.of(2015, 10, 19), 22.0, 30.0));
         dept.addEmployee(new HourlyEmployee("Frannie", LocalDate.of(2022, 9, 10), 25.0, 30.0));
-        dept.payEmployees();
+        dept.addEmployee(new Executive("Jay", LocalDate.of(1986, 12, 5), 100_000.0));
 
         // list its Employees
         System.out.println("\nList employees:");
@@ -42,5 +40,9 @@ class HRClient {
 
         System.out.println("\nPay employees:");
         dept.payEmployees();
+
+        // forced holiday break
+        System.out.println("\nForced holiday break:");
+        dept.holidayBreak();
     }
 }
