c42a4b508618b58a5efb7eb6fc20b9ab945e1fee
Jelani Poole <jelani.poole@tlgcohort.com>
2023-10-27 12:41:03 -0700

    Boat.moveTo (no exception thrown)

---
+++ Lab10-Extra/src/com/transportation/BoatClient.java
@@ -0,0 +1,10 @@
+package com.transportation;
+
+class BoatClient {
+
+    public static void main(String[] args) {
+        Boat boat = new Boat("ABCE90");
+        boat.moveTo(" West Seattle");
+    }
+
+}
